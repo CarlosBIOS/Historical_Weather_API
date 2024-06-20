80999dd81089c80a5ebd6774cc5d707f92fdd970
Carlos Monteiro <cmmonteiro40@gmail.com>
2024-06-20 12:53:36 +0100

    Initial commit

--- main.py
+++ main.py
@@ -1,23 +1,50 @@
 from flask import Flask, render_template
 import pandas as pd
+import numpy
 
 app = Flask(__name__)  # É como fazem os profissionais, colocam __name__ em vez de 'word'
+stations = pd.read_csv('data_small/stations.txt', skiprows=17)[:183][['STAID',
+                                                                      'STANAME                                 ']]
+# Vou criar uma varoável chamada stations que servirá para mostrar uma tabela sobre todos os stations disponiveis. Para
+# isso, vou ter que usar o read_csv e dps to_html e coloquei só até 101, devido ao data_small. No ficheiro home.html,
+# tenho que acescentar este código:
+# <p>{{data|safe}}</p>, onde safe cria uma tabela formatada!!!
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', data=stations.to_html())
 
 
 @app.route('/api/v1/<station>/<date>')
 def station_date(station, date):
-    print('data_small/TG_STAID' + str(station).zfill(6) + '.txt')
     data_frame = pd.read_csv('data_small/TG_STAID' + str(station).zfill(6) + '.txt', skiprows=20,
                              parse_dates=['    DATE'])
     temperature = data_frame.loc[data_frame['    DATE'] == date]['   TG'].squeeze() / 10
-    return {'station': station,
-            'date': date,
-            'temperature': temperature}
+    return {
+        'station': stations.loc[stations['STAID'] == int(station)]['STANAME                                 ']
+        .squeeze().strip(),
+        'date': f'{date[len(date)-2:]}-{date[4:6]}-{date[:4]}',
+        'temperature': temperature}
+
+
+@app.route('/api/v1/<station_number>')
+def all_data(station_number):
+    data_frame = pd.read_csv('data_small/TG_STAID' + str(station_number).zfill(6) + '.txt', skiprows=20,
+                             parse_dates=['    DATE'])[['    DATE', '   TG']]
+    data_frame['   TG'] = data_frame['   TG'].mask(data_frame['   TG'] == -9999, numpy.nan) / 10
+    number = (stations.loc[stations['STAID'] == int(station_number)]['STANAME                                 '].squeeze()
+              .strip())
+    return render_template('station.html', station=data_frame.to_html(),
+                           text=f'Here is the information about the station {station_number}: {number}')
+
+
+@app.route('/api/v1/<station_number>/<year>')
+def yearly(station_number, year):
+    data_frame = pd.read_csv('data_small/TG_STAID' + str(station_number).zfill(6) + '.txt', skiprows=20,
+                             parse_dates=['    DATE'])[['    DATE', '   TG']].to_html()
+    return render_template('station.html', station=data_frame, text=f'Here is the information about the station '
+                                                                    f'{station_number}')
 
 
 @app.route('/tutorial')
